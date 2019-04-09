f36f7f5e9034f5173fff32c9eecf2edeb3384469
Panni <simaianna3@gmail.com>
2019-04-09 23:09:04 +0200

    docs

---
+++ docs/precache-manifest.a7f6ec3073c0c7b92f966756a1d5f31e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6baceac55294d2280448",
+    "url": "/static/js/main.a000237c.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6baceac55294d2280448",
+    "url": "/static/css/main.4bd5de49.chunk.css"
+  },
+  {
+    "revision": "70080951aba50df83564c363eb18c471",
+    "url": "/index.html"
+  }
+];
